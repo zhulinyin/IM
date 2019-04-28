60960bc27df068de916063d7e3f65049496a5725
zhulinyin <lijt26@mail2.sysu.edu.cn>
2019-04-28 16:19:24 +0800

    重新组织了文件结构

---
+++ IM/Main/InfoPage/InfoViewController.h
@@ -0,0 +1,17 @@
+//
+//  InfoViewController.h
+//  IM
+//
+//  Created by zhulinyin on 2019/4/27.
+//  Copyright © 2019 zhulinyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InfoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IM/Main/InfoPage/InfoViewController.m
@@ -0,0 +1,32 @@
+//
+//  InfoViewController.m
+//  IM
+//
+//  Created by zhulinyin on 2019/4/27.
+//  Copyright © 2019 zhulinyin. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
