bbbe946ad71166cdd3b7f760e79b638b6bc79d19
dick <dick@dickdeMac.local>
2019-05-12 10:11:16 -0700

    暂存代码，用列表修改个人详情页

--- IM/Main/InfoPage/InfoViewController.m
+++ IM/Main/InfoPage/InfoViewController.m
@@ -8,11 +8,19 @@
 
 #import "InfoViewController.h"
 
-@interface InfoViewController ()
+@interface InfoViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property(nonatomic, strong) UITableView *tableView;
+@property(nonatomic, strong) NSMutableArray<NSString*> *titleList;
+@property(nonatomic, strong) NSMutableArray<NSString*> *contentList;
 
 @end
 
 @implementation InfoViewController
+
+- (instancetype) init_self {
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -31,7 +39,66 @@
     [self.Gender sizeToFit];
     [self.Birthplace sizeToFit];*/
     
+    self.navigationItem.title = @"个人信息";
+    
+    self.tableView = ({
+        UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        tableView;
+    });
+    // [self.view addSubview:self.tableView];
+    
+    [self loadData];
 }
+
+- (void)loadData {
+    self.contentList = [NSMutableArray array];
+    [self.contentList addObjectsFromArray:[[NSArray alloc] initWithObjects:@"头像", @"昵称", @"账号", @"性别", @"地区",nil]];
+    self.titleList = [NSMutableArray array];
+    [self.titleList addObjectsFromArray:[[NSArray alloc] initWithObjects:@"小猪佩奇", @"Peppa", @"peppy", @"female", @"UK",nil]];
+}
+
+#pragma mark ------------ UITableViewDataSource ------------------
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.contentList.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *cellID = [NSString stringWithFormat:@"cellID:%zd", indexPath.section];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (nil == cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    
+    //        cell.imageView.image = [UIImage imageNamed:@"icon_directory.jpg"];
+    cell.textLabel.text = self.contentList[indexPath.row];
+    
+    UILabel *rightLabel = [[UILabel alloc]initWithFrame:CGRectMake(0,0,70,45)];
+    rightLabel.text = self.titleList[indexPath.row];
+    cell.accessoryView = rightLabel;
+    cell.accessoryView.backgroundColor = [UIColor redColor];   //加上红色容易看清楚
+    
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
+}
+
+#pragma mark ------------ UITableViewDelegate ------------------
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    InfoViewController *controller = [[InfoViewController alloc] init];
+    controller.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:controller animated:YES];
+}
+
+
 - (IBAction)logout:(id)sender {
     NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:defaultConfigObject delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
