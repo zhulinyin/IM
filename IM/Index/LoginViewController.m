32602a6e879d9d78221f495ffa652aac4f09d253
student9 <student9@student9s-mac-mini.local>
2019-06-15 12:00:23 +0800

    add login avatar

--- IM/Index/LoginViewController.m
+++ IM/Index/LoginViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *passwordText; // 密码输入文本框
 @property (weak, nonatomic) IBOutlet UIButton *loginBtn;    // 登录按钮
 @property (weak, nonatomic) IBOutlet UIButton *registerBtn; // 注册按钮
+@property (weak, nonatomic) IBOutlet UIImageView *userLoginAvatar;
 
 @end
 
@@ -30,6 +31,12 @@
     [[UserManager getInstance] login:self.usernameText.text withPassword:self.passwordText.text];
 }
 - (IBAction)usernameInputDone:(id)sender {
+    
+    if ([_usernameText.text  isEqual: @""]) {
+        _userLoginAvatar.image = [UIImage imageNamed:@"peppa"];
+        return;
+    }
+    
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     NSString *url = [URLHelper getURLwithPath:[[NSString alloc] initWithFormat:@"/account/info/user/%@", _usernameText.text]];
     
@@ -37,15 +44,25 @@
         if([responseObject[@"state"] isEqualToString:@"ok"])
         {
             NSLog(@"get Info success");
+            [_userLoginAvatar sd_setImageWithURL:[NSURL URLWithString:[[NSString alloc] initWithFormat:@"http://118.89.65.154:8000%@", responseObject[@"data"][@"Avatar"]]]
+                placeholderImage:[UIImage imageNamed:@"peppa"]
+               completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+                   NSLog(@"error== %@",error);
+                   if (error) {
+                       _userLoginAvatar.image = [UIImage imageNamed:@"peppa"];
+                   }
+               }];
         }
         else
         {
             NSLog(@"%@", responseObject[@"msg"]);
-            NSLog(@"modifyInfo fail");
+            _userLoginAvatar.image = [UIImage imageNamed:@"peppa"];
+            NSLog(@"get Info fail1");
         }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"get Info fail");
+        NSLog(@"get Info fail2");
         NSLog(@"%@", error.localizedDescription);
+        _userLoginAvatar.image = [UIImage imageNamed:@"peppa"];
     }];
 }
 
